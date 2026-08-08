befeb2087edacc9d2b21e6028c7480bf89d88dca
Valentin Yeo <valentin.yeo@gmail.com>
2026-08-08 23:10:52 +0200

    Fix recency test import

--- app/src/test/java/dev/multiprompt/companion/data/SessionSearchTest.kt
+++ app/src/test/java/dev/multiprompt/companion/data/SessionSearchTest.kt
@@ -1,6 +1,7 @@
 package dev.multiprompt.companion.data
 
 import dev.multiprompt.companion.model.TmuxSession
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
